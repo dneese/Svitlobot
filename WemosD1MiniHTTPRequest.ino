840ac667c3ce79dc10ba7e8754c359d51cde771d
dneese <67241825+dneese@users.noreply.github.com>
2024-08-01 04:02:13 +0300

    Update WemosD1MiniHTTPRequest.ino

--- WemosD1MiniHTTPRequest.ino
+++ WemosD1MiniHTTPRequest.ino
@@ -1,15 +1,19 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
-#include <TimerOne.h>
+#include <Ticker.h>
 
 // WiFi налаштування
 const char* ssid = "ВАШ_ССІД";
 const char* password = "ВАШ_ПАРОЛЬ";
 
-// Інтервал між запитами в мілісекундах (4 хвилини)
-const unsigned long interval = 240000;
+// Інтервал між запитами в секундах (4 хвилини)
+const unsigned long interval = 240; // в секундах
 const String channelKey = "ВАШКЛЮЧ";
-const String serverURL = "http://api.svitlobot.in.ua/channelPing?channel_key=" + channelKey;
+const String serverURL = "https://api.svitlobot.in.ua/channelPing?channel_key=" + channelKey;
+const String internetCheckURL = "http://www.google.com"; // URL для перевірки доступності інтернету
+
+Ticker wifiCheckTimer;
+Ticker httpRequestTimer;
 
 void setup() {
   Serial.begin(9600);                  // Ініціалізація серійного зв'язку
@@ -24,16 +28,35 @@ void setup() {
   Serial.println("IP адреса: ");
   Serial.println(WiFi.localIP());
 
-  Timer1.initialize(interval);         // Ініціалізація таймера
-  Timer1.attachInterrupt(makeHTTPRequest); // Прив'язування переривання
+  wifiCheckTimer.attach(10, checkWiFi); // Перевірка статусу WiFi кожні 10 секунд
+  httpRequestTimer.attach(interval, makeHTTPRequest); // Встановлення таймера для HTTP-запитів
 }
 
 void loop() {
   // Порожня функція loop
 }
 
+void checkWiFi() {
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.println("Спроба перепідключення до WiFi...");
+    WiFi.begin(ssid, password);
+    if (WiFi.status() == WL_CONNECTED) {
+      Serial.println("Підключено до WiFi");
+      Serial.println("IP адреса: ");
+      Serial.println(WiFi.localIP());
+    }
+  }
+}
+
 void makeHTTPRequest() {
   if (WiFi.status() == WL_CONNECTED) {
+    if (!checkInternetConnection()) {
+      Serial.println("Інтернет недоступний, спроба перепідключення...");
+      WiFi.disconnect();
+      WiFi.begin(ssid, password);
+      return;
+    }
+
     HTTPClient http;
 
     Serial.print("Відправка HTTP-запиту на: ");
@@ -47,11 +70,20 @@ void makeHTTPRequest() {
       Serial.println(httpCode);      // Виведення статусного коду
       Serial.println(payload);       // Виведення відповіді
     } else {
-      Serial.println("Помилка HTTP-запиту");
+      Serial.print("Помилка HTTP-запиту: ");
+      Serial.println(http.errorToString(httpCode).c_str()); // Виведення тексту помилки
     }
 
     http.end(); // Завершення запиту
   } else {
     Serial.println("Не підключено до WiFi");
   }
+}
+
+bool checkInternetConnection() {
+  HTTPClient http;
+  http.begin(internetCheckURL);
+  int httpCode = http.GET();
+  http.end();
+  return httpCode == HTTP_CODE_OK;
 }
